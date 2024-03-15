c1e45a1f71a6a70aee66917af1e2eb091e78ecd5
carlos <carlos@localhost.github>
2024-03-15 03:56:22 -0400

    Add middleware and fuction to handle errors

--- api/index.js
+++ api/index.js
@@ -24,5 +24,15 @@ app.listen(3000, () =>{
     console.log("sever is running on port 3000 now")
 })
 
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
